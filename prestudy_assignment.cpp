d035aa409ffd4a47a780e4ef1e0c4fe91aafa0a0
heidikrisse <heidi.kristiina90@gmail.com>
2023-05-15 09:21:34 +0300

    corrected
    Added size type for the vector and removed unnecessary variables average and difference

--- prestudy_assignment.cpp
+++ prestudy_assignment.cpp
@@ -5,57 +5,66 @@
 
 using namespace std;
 
-int main() {
-    // variable for the number of integers (from the file)
-    int n{};
+int main()
+{
+    // The size type for a vector of integers
+    vector<int>::size_type n{};
     cout << "Enter the number of integers: ";
     cin >> n;
 
-    // vector to hold the numbers (size n)
-    // vector is chosen because the size of the list is not known beforehand before the users' input
+    // Vector to store the numbers from the file
     vector<int> numbers(n);
 
-    // open the file
+    // Open the file for reading
     ifstream file("numbers.txt");
-    for (int i {0}; i < n; i++) {
+    for (vector<int>::size_type i{0}; i < n; i++)
+    {
         file >> numbers[i];
     }
     file.close();
 
-    int sum {0};
-    int min_value {numbers[0]};
-    int max_value {numbers[0]};
-    int mode_value {numbers[0]};
-    int mode_count {1};
-    for (int i = {0}; i < n; i++) {
+    int sum{0};
+    int min_value{numbers[0]};
+    int max_value{numbers[0]};
+    int mode_value{numbers[0]};
+    int mode_count{1};
+    for (vector<int>::size_type i = {0}; i < n; i++)
+    {
         sum += numbers[i];
-        if (numbers[i] < min_value) {
+
+        if (numbers[i] < min_value)
+        {
             min_value = numbers[i];
         }
-        if (numbers[i] > max_value) {
+
+        if (numbers[i] > max_value)
+        {
             max_value = numbers[i];
         }
-        // initialize (copy list initialization) a count for the number of times the value appears in the array
-        int count {0};
-        for (int j {0}; j < n; j++) {
-            if (numbers[j] == numbers[i]) {
-                count++;
+
+        // Variable to store the count of the current number
+        int numberCount{0};
+        for (vector<int>::size_type j{0}; j < n; j++)
+        {
+            if (numbers[j] == numbers[i])
+            {
+                numberCount++;
             }
         }
-        if (count > mode_count) {
-            mode_count = count;
+
+        if (numberCount > mode_count)
+        {
+            mode_count = numberCount;
             mode_value = numbers[i];
         }
     }
-    double average = {double(sum) / double(n)};
-    int difference = {max_value - min_value};
 
     cout << "The sum of the integers in the list: " << sum << '\n';
-    cout << "The average of the integers in the list: " << average << '\n';
+    cout << "The average of the integers in the list: " << double(sum) / (double)n << '\n';
     cout << "The minimum value in the list: " << min_value << '\n';
     cout << "The maximum value in the list: " << max_value << '\n';
     cout << "The most frequent value in the list: " << mode_value << '\n';
-    cout << "Difference between the min and max of the list: " << difference << '\n';
+    cout << "Difference between the min and max of the list: " << max_value - min_value << '\n';
 
     return 0;
 }
